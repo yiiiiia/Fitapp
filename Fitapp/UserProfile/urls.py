aa20818a7e51a7c4cda6e00c828fb5355e43b2d2
Zixiao Wang <2928605w@student.gla.ac.uk>
2024-03-04 13:40:24 +0000

    add update_profile

--- Fitapp/UserProfile/urls.py
+++ Fitapp/UserProfile/urls.py
@@ -1,8 +1,11 @@
 # UserProfile/urls.py
 from django.urls import path
-from .views import LoginView, SignupView
+from . import views
+from .views import LoginView, SignupView, UserProfileUpdateView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('profile', UserProfileUpdateView.as_view(), name='profile-update'),
+    path('update_profile/', views.update_profile, name='update_profile'),
 ]
--- Fitapp/UserProfile/views.py
+++ Fitapp/UserProfile/views.py
@@ -5,7 +5,10 @@ from django.contrib.auth import authenticate
 from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from .models import UserProfile
 from .models import AuthToken
+from django.shortcuts import render
 import logging
 
 logger = logging.getLogger(__name__)
@@ -52,3 +55,20 @@ class SignupView(APIView):
         user.save()
 
         return JsonResponse({'message': 'User created successfully'}, status=201)
+    
+def update_profile(request):
+    return render(request, 'update_profile.html')
+
+class UserProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+        user_profile.gender = request.data.get('gender')
+        user_profile.age = request.data.get('age')
+        user_profile.height = request.data.get('height')
+        user_profile.weight = request.data.get('weight')
+        logger.error(user_profile.user)
+        user_profile.save()
+
+        return Response({'message': 'Profile updated successfully'}, status=200)
